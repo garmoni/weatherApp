905bd0e2c5610056a07ecb2a3376116d5b350036
Ольга Цыб <dev01@varius.local>
2021-08-12 14:52:49 +0300

    combining functions

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,7 +7,7 @@ import { apiKeys, dateFormat } from '../constant/constant';
 import moment from 'moment';
 
 import './Styles.css';
-import { Preloader } from './loader';
+// import { Preloader } from './loader';
 
 export const Main = () => {
     const lang = !JSON.parse(localStorage.getItem('lang')) ? "en" : JSON.parse(localStorage.getItem('lang'));
@@ -17,7 +17,6 @@ export const Main = () => {
     const [select, setSelect] = useState(lang);
     const [input, setInput] = useState('')
     const [cityName, setCityName] = useState('')
-    const [list, setList] = useState()
     const date = new Date()
 
     const handleChange = (event) => {
@@ -53,58 +52,30 @@ export const Main = () => {
     }, [cards, select])
 
     const getWeather = async (e) => {
-        dataTime()
         e.preventDefault();
         setCityName(input)
         setInput('')
         try {
+            const responseList = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${apiKeys}&units=metric`);
+            console.log(responseList)
+            var filterArr = responseList.data.list.filter(function (number) {
+                return moment(date).isSame(number.dt_txt, 'day')
+            });
+            var listArr = filterArr.map(function (item) {
+                return { name: moment(item.dt_txt).format(dateFormat.TIME), value: Math.floor(item.main.temp) }
+            });
+            console.log("function dataTime - listArr", listArr)
             const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${apiKeys}&lang=${select}`);
-            addWeather(response.data)
+            const newItem = {
+                id: Math.random().toString(15),
+                card: response.data,
+                date: moment(date).format(dateFormat.DATE_TIME),
+                graph: listArr,
+            } 
+            setCards([...cards, newItem])
         } catch (error) {
             setError('Error');
         }
-    }
-
-    const dataTime = () => {
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${apiKeys}&units=metric`)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    var filterArr = result.list.filter(function (number) {
-                        return moment(date).isSame(number.dt_txt, 'day')
-                    });
-                    var listArr = filterArr.map(function (item) {
-                        return { name: moment(item.dt_txt).format(dateFormat.TIME), value: Math.floor(item.main.temp) }
-                    });
-                    console.log("function dataTime - listArr", listArr)
-                    return setList(listArr)
-                },
-                (error) => {
-                }
-            )
-    }
-    // const hourlyData = (list) => {
-    //     var filterArr = list.filter(function (number) {
-    //         return moment(date).isSame(number.dt_txt, 'day')
-    //     });
-    //     var listArr = filterArr.map(function (item) {
-    //         return { name: moment(item.dt_txt).format(dateFormat.TIME), value: Math.floor(item.main.temp) }
-    //     });
-    //     console.log(listArr)
-    //     return listArr;
-    // }
-
-    const addWeather = (data) => {
-        console.log("function addWeather - listArr", list)
-        if (list.length && data) {
-            const newItem = {
-                id: Math.random().toString(15),
-                card: data,
-                date: moment(date).format(dateFormat.DATE_TIME),
-                graph: list,
-            } 
-            setCards([...cards, newItem])
-        } else <img src="../../img/loader.gif" alt="loader" />
     }
 
     const removeCards = (id) => {
